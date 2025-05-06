618ab94004d51ffa263d93fbc913d9b3152f55d0
Bruno Quintino <brunoquintino.tds2.senai@gmail.com>
2025-05-06 08:48:32 -0300

    feat: substituindo placeholders 1/2

--- app/components/buttons/button.jsx
+++ app/components/buttons/button.jsx
@@ -1,10 +1,15 @@
+import { Link } from "expo-router";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 export default function Botao() {
     return (
         <View style={styles.container}>
             <TouchableOpacity style={styles.card}>
+            <Link href="https://www.iszi.com.br/quiz?gad_source=1&gad_campaignid=17890980904&gbraid=0AAAAABejgmz3UMfMDXp_oRsfXJDG_s-ia&gclid=Cj0KCQjw5ubABhDIARIsAHMighYrOL8E2lDeV0QV5CdtfODpgpURdkInRCvtyI7zSFK8WE4TSHfpqgwaAomVEALw_wcB" asChild>
                 <Text style={styles.text}>Quiz</Text>
+            </Link>
+           
+                    
             </TouchableOpacity>
 
             <TouchableOpacity style={styles.card}>
